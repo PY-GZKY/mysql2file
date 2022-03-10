126bca02760d575b05fba19dd61b41eb8b87695e
PY-GZKY <341796767@qq.com>
2022-03-10 16:44:07 +0800

    Create test_single.py

---
+++ tests/test_single.py
@@ -0,0 +1,54 @@
+import dotenv
+import pytest
+
+from mysql2file import MysqlEngine
+
+dotenv.load_dotenv(verbose=True)
+
+
+def setup_function():
+    global M
+    M = MysqlEngine()
+
+
+def test_to_csv():
+    result_ = M.to_csv(folder_path="_csv",is_block=False, block_size=50000)
+    print(result_)
+    assert "successfully" in result_
+
+
+def test_to_excel():
+    result_ = M.to_excel(folder_path="_excel", is_block=False, block_size=50000, mode='xlsx', ignore_error=True)
+    print(result_)
+    assert "successfully" in result_
+
+
+def test_to_json():
+    result_ = M.to_json(folder_path="./_json", is_block=False, block_size=50000)
+    print(result_)
+    assert "successfully" in result_
+
+def test_to_pickle():
+    result_ = M.to_pickle(folder_path="./_pickle")
+    print(result_)
+    assert "successfully" in result_
+
+
+def test_to_feather():
+    result_ = M.to_feather(folder_path="./_feather")
+    print(result_)
+    assert "successfully" in result_
+
+
+def test_to_parquet():
+    result_ = M.to_parquet(folder_path="./_parquet")
+    print(result_)
+    assert "successfully" in result_
+
+
+def teardown_function():
+    ...
+
+
+if __name__ == "__main__":
+    pytest.main(["-s", "test_single.py"])

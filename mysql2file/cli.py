2defece735da0f263d59d8db37ae64d969049e93
PY-GZKY <341796767@qq.com>
2022-03-11 16:11:13 +0800

    Fix: cli 对此意义不大，故放弃

--- mysql2file/cli.py
+++ mysql2file/cli.py
@@ -1,15 +1,10 @@
 import click
-from colorama import init
-
-from .version import __version__
-
-init(autoreset=True)
+from version import __version__
 
 @click.command("mysql2file")
 @click.version_option(__version__, '-V', '--version', prog_name='mysql2file')
 def cli():
     ...
-
 
 if __name__ == '__main__':
     cli()
